4efdcbbf7ed406c7c84a5bb6a282c68163537a2d
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-03-18 18:16:43 +0100

    movimento ball e primi brick

--- app.js
+++ app.js
@@ -8,30 +8,108 @@ let yEnd = playGround.getBoundingClientRect().bottom
 let width = playGround.getBoundingClientRect().width
 let height = playGround.getBoundingClientRect().height
 
+
+let timderId
 //console.log('Width: ' + width)
+//console.log('xStart: '+xStart)
+
+
+//Creazione pad
 
 let pad = document.createElement('div')
-pad.classList.add('pad')
+pad.classList.add('pad', 'd-flex', 'justify-content-between')
+pad.innerHTML =`
+<div class="corner"></div>
+<div class="corner"></div>
+`
 
-let xPosition = width/2
-//console.log('xPosition: '+xPosition)
-drowPad()
+playGround.append(pad)
+
+padWidth = pad.getBoundingClientRect().width
+//docWidth = playGround.innerWidth
+
+//console.log('pad: '+padWidth)
+
+padHeight = pad.getBoundingClientRect().height
 pad.style.top = ((height-padHeight*3) +'px')
+let padPosition = width/2
+pad.style.left = padPosition +'px'
+
+
+//create a brick
+brickWidth = width*0.15
+brickHeight = height*0.05
+console.log('brickWidth:' + brickWidth)
+console.log('brickHeight:' + brickHeight)
+
+
+class Brick{
+    constructor(x, y){
+        this.bottomLeft =[x, y]
+        this.bottomRight = [x + brickWidth, y]
+        this.topLeft = [x, y + brickHeight]
+        this.topRight = [x + brickWidth, y + brickHeight]
+    }
+}
+
+let bricks =[
+    new Brick(width*0.05, height*0.05),
+    new Brick(width*0.22, height*0.05),
+    new Brick(width*0.39, height*0.05),
+    new Brick(width*0.56, height*0.05),
+    new Brick(width*0.73, height*0.05),
+    
+    
+]
+
+
+function addBricks(){
+    for(let i=0; i<bricks.length; i++){
+        let brick = document.createElement('div')
+        brick.classList.add('brick')
+       
+        brick.style.left = bricks[i].bottomLeft[0] + 'px'
+        brick.style.top = bricks[i].bottomLeft[1] + 'px'
+        
+        playGround.append(brick)
+        // console.log('brick: '+ i)
+        // console.log(bricks[i].bottomLeft[0])
+        // console.log(bricks[i].bottomLeft[1])
+
+    }
+}
+
+
+addBricks()
+
+
+
+
+
+
+
+
+
+
 
 document.addEventListener('keydown', movePadKey)
 playGround.addEventListener('mousemove', movePadMouse);
-//console.log('Pad: ' + padWidth)
 //command.addEventListener('pointerdown',  movePadCommand)
+
 
 //Creazione ball
 let ball = document.createElement('div')
 ball.setAttribute('id', 'ball')
-const ballStart = [width/2, (height - padHeight*5 -5)]
+
+xDirection = 5
+yDirection = 5
+
+
+const ballStart = [(width/2-10), (height - padHeight*4 -5)]
 let ballCurrentPosition = ballStart
-
-
-//playGround.append(ball)
 drowBall()
+let ballDiametro = ball.getBoundingClientRect().width
+
 
 document.addEventListener('click', ballMove)
 
@@ -47,26 +125,24 @@ document.addEventListener('click', ballMove)
 
 function drowPad(){
     
-    pad.style.left = xPosition +'px'
     playGround.append(pad)
-    padWidth = pad.getBoundingClientRect().width
-    padHeight = pad.getBoundingClientRect().height
-
-
+    pad.style.left = padPosition +'px'
 }
 
 function movePadKey(event){
-    if(event.key == 'ArrowLeft' && xPosition > 0){   
-        xPosition = xPosition - 50
-        if(xPosition < padWidth/2) {xPosition = padWidth/2}
+    if(event.key == 'ArrowLeft' && padPosition > 0){   
+         
+        padPosition = padPosition - 50
+        if(padPosition < padWidth/2) {padPosition = padWidth/2}
         
         pad.remove()
         drowPad()
     }
     
-    if(event.key == 'ArrowRight' && xPosition < width - padWidth/2){    
-        xPosition = xPosition + 50  
-        if(xPosition > width - padWidth/2) {xPosition = width - padWidth/2} 
+    if(event.key == 'ArrowRight' && padPosition < width - padWidth/2){    
+        
+        padPosition = padPosition + 50      
+        if(padPosition > width - padWidth/2) {padPosition = width - padWidth/2} 
         
         pad.remove()
         drowPad()
@@ -76,14 +152,19 @@ function movePadKey(event){
 }
 
 function movePadMouse(event) {
-    let cursore = event.clientX - xStart;
+    let pointer = event.clientX - xStart;
     
-    if (cursore < padWidth/2){
+    if (pointer < padWidth/2){
         pad.style.left = (padWidth/2  + 'px');
-    } else if(cursore > width-padWidth/2){
+    } else if(pointer > width-padWidth/2){
         pad.style.left = (width-padWidth/2  + 'px');
     }else{
-        pad.style.left = (cursore  + 'px');
+        padPosition = pointer;
+        pad.remove()
+        drowPad()
+        //console.log(Math.floor(padPosition - padWidth/2), Math.floor(padPosition + padWidth/2))
+        
+        
     }      
 }
 
@@ -140,22 +221,91 @@ function drowBall(){
     playGround.append(ball)
 }
 
-
 function ballMove(){
-    setInterval(move, 50)
+    document.removeEventListener('click', ballMove)
+    timerId = setInterval(move, 20)
 }
+
 function move(){
+    checkCollision()
+    ballCurrentPosition[0] += xDirection 
+    ballCurrentPosition[1] -= yDirection    
     
-    ballCurrentPosition[0] +=5 
-    ballCurrentPosition[1] -=5
+    ball.remove()
+    drowBall()  
+}
 
-        // ball.style.left = ((ball.style.left +5) + 'px')
-        // ball.style.top = ((ball.style.top -5) + 'px')
-        ball.remove()
-        drowBall()
+function checkCollision(){
+    let data = document.querySelector('#data')
+    data.innerHTML = `x: ${xDirection}      y: ${yDirection}` 
+    // console.log('x: '+xDirection)
+    // console.log('y: '+ yDirection)
+    
+    if(ballCurrentPosition[0]>= width-ballDiametro || ballCurrentPosition[0] <= 0){
+        changeXDirection()
+    }
 
+    if(ballCurrentPosition[1]<= 0){
+        changeYDirection()
+    }
+
+    // Pad Bounce
+    if((ballCurrentPosition[1] > (height-padHeight*4 - 3))
+    && (ballCurrentPosition[0] >= (padPosition - padWidth/2 + 20))
+    && (ballCurrentPosition[0] <= (padPosition + padWidth/2 - 20))){
+        changeYDirection()
+    }
     
-    
+    // Pad Left corner bounce
+    if((ballCurrentPosition[1] > (height-padHeight*4 - 3))
+    && (ballCurrentPosition[0] >= (padPosition - padWidth/2))
+    && (ballCurrentPosition[0] < (padPosition - padWidth/2 + 20))){
+        console.log('left corner')
+        
+        if(xDirection>=0){
+            xDirection += 1
+            yDirection += 1
+           
+        }else{
+            xDirection += -1
+            yDirection += 1 
+        }
+        
+        changeYDirection()
+    }
+
+    // Pad Right corner bounce
+    if((ballCurrentPosition[1] > (height-padHeight*4 - 3))
+    && (ballCurrentPosition[0] > (padPosition + padWidth/2 -20))
+    && (ballCurrentPosition[0] <= (padPosition + padWidth/2))){
+        console.log('right corner')
+
+        if(xDirection>=0){
+            xDirection += 1
+            yDirection += 1
+            
+        }else{
+            xDirection += -1
+            yDirection += 1 
+        }
+
+        changeYDirection()
+    }
+
+
+    // Lost
+    if(ballCurrentPosition[1] >= height-30){
+        clearInterval(timerId)
+    }
+
+}
+
+function changeXDirection(){
+    xDirection = - xDirection
+}
+
+function changeYDirection(){
+    yDirection = - yDirection
 }
 
 
